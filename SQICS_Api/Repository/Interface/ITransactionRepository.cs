e3ca64561a0e092592d17dd99d50209733438308
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-15 15:50:26 +0800

    added TransactionRepo

---
+++ SQICS_Api/Repository/Interface/ITransactionRepository.cs
@@ -0,0 +1,13 @@
+﻿using SQICS_Api.Model;
+using SQICS_Api.Repository.Base.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.Interface
+{
+    public interface ITransactionRepository : IRepositoryBase<tbl_t_transaction>
+    {
+    }
+}
---
+++ SQICS_Api/Repository/Transaction/TransactionRepository.cs
@@ -0,0 +1,21 @@
+﻿using SQICS_Api.Model;
+using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Base;
+using SQICS_Api.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.Transaction
+{
+    public class TransactionRepository : RepositoryBase<tbl_t_transaction>, ITransactionRepository
+    {
+        public TransactionRepository(SQICSContext context) : base(context){}
+
+        public async Task<IEnumerable<tbl_t_transaction>> GetAllTransaction()
+        {
+            return await QueryAsync("select * from tbl_t_transaction");
+        }
+    }
+}
