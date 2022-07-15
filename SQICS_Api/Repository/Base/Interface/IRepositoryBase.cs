715370aaa25842d29725bb3350ea82f6e28e2837
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-15 15:35:00 +0800

    added IRepositoryBase

---
+++ SQICS_Api/Repository/Base/Interface/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.Base.Interface
+{
+    public interface IRepositoryBase<T>
+    {
+        Task<IEnumerable<T>> QueryAsync();
+
+    }
+}
