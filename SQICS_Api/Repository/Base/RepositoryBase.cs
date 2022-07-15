c244e016948b98da50cd0eadc3e968a34497bf34
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-15 15:43:25 +0800

    Repositorybase added QueryAsync

--- SQICS_Api/Repository/Base/RepositoryBase.cs
+++ SQICS_Api/Repository/Base/RepositoryBase.cs
@@ -1,4 +1,6 @@
-﻿using SQICS_Api.Repository.Base.Interface;
+﻿using Dapper;
+using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Base.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +8,20 @@ using System.Threading.Tasks;
 
 namespace SQICS_Api.Repository.Base
 {
-    public class RepositoryBase<T> : IRepositoryBase<T> where T : class 
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
     {
+        SQICSContext _context;
 
+        public RepositoryBase(SQICSContext context)
+        {
+            _context = context;
+        }
+        public Task<IEnumerable<T>> QueryAsync(string query, object param = null)
+        {
+            using (var conn = _context.Connection)
+            {
+                return conn.QueryAsync<T>(query, param);
+            }
+        }
     }
 }
