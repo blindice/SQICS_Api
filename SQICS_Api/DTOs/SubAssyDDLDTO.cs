c1dcebb5902a4392afb59cc29c89a54c9c78f0fd
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-18 15:02:19 +0800

    added subassyDLLDTO

---
+++ SQICS_Api/DTOs/SubAssyDDLDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Model
+{
+    public class SubAssyDDLDTO
+    {
+        public string SubAssyCode { get; set; }
+
+        public int Id { get; set; }
+    }
+}
