d6d6de80284b882d700fbec4cc6ea7aab425ef63
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-18 11:45:55 +0800

    added planDTO

---
+++ SQICS_Api/DTOs/PlanDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class PlanDTO
+    {
+        public int Id { get; set; }
+
+        public int TransactionNo { get; set; }
+
+        public string SubAssyCode { get; set; }
+
+        public string SubAssyName { get; set; }
+
+        public int? Qty { get; set; }
+
+        public DateTime? ETimeCompletion { get; set; } 
+    }
+}
