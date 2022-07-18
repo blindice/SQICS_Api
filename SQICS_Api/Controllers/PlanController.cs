baad0f0d289d150bfb88f59c6e57b5e818de5415
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-18 11:46:02 +0800

    added plan controller

---
+++ SQICS_Api/Controllers/PlanController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQICS_Api.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlanController : ControllerBase
+    {
+        IPlanService _service;
+        public PlanController(IPlanService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPlanAsync()
+        {
+            var plans = await _service.GetAllPlanAsync();
+
+            return Ok(plans);
+        }
+    }
+}
