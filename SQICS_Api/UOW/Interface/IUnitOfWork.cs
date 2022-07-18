c5c9390e5cc3a3fb08ff0229c06b7c42557b43eb
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-18 10:25:06 +0800

    added Unitofwork

---
+++ SQICS_Api/UOW/Interface/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.UOW
+{
+    public interface IUnitOfWork
+    {
+        ITransactionRepository Transaction { get; }
+
+        ISubAssyRepository SubAssy { get; }
+
+        Task Save();
+    }
+}
---
+++ SQICS_Api/UOW/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Interface;
+using SQICS_Api.Repository.SubAssy;
+using SQICS_Api.Repository.Transaction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.UOW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private SQICSContext _context;
+        private ITransactionRepository _transaction;
+        private ISubAssyRepository _subAssy;
+
+        public UnitOfWork(SQICSContext context) => _context = context;
+
+        public ITransactionRepository Transaction
+        {
+            get
+            {
+                if (_transaction == null)
+                {
+                    _transaction = new TransactionRepository(_context);
+                }
+                return _transaction;
+            }
+        }
+
+        public ISubAssyRepository SubAssy
+        {
+            get
+            {
+                if (_subAssy == null)
+                {
+                    _subAssy = new SubAssyRepository(_context);
+                }
+                return _subAssy;
+            }
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
