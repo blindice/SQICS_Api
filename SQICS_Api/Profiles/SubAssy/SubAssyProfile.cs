75e809b00056739eb93858d5c8bb04ba7fcb4a91
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-18 15:02:47 +0800

    added subassy mapping profile

---
+++ SQICS_Api/Profiles/SubAssy/SubAssyProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using SQICS_Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Profiles.SubAssy
+{
+    public class SubAssyProfile : Profile
+    {
+        public SubAssyProfile()
+        {
+            CreateMap<tbl_m_part, SubAssyDDLDTO>()
+                .ForMember(dest => dest.SubAssyCode,
+                opt => opt.MapFrom(src => src.fld_partCode))
+                 .ForMember(dest => dest.Id,
+                opt => opt.MapFrom(src => src.fld_id)).ReverseMap();
+        }
+    }
+}
