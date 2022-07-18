39a6fddb9185f7736b8a4f0499376f6c05e65ca6
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-18 08:11:21 +0800

    registered transaction Repo

--- SQICS_Api/Startup.cs
+++ SQICS_Api/Startup.cs
@@ -9,6 +9,10 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Base;
+using SQICS_Api.Repository.Base.Interface;
+using SQICS_Api.Repository.Interface;
+using SQICS_Api.Repository.Transaction;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +35,7 @@ namespace SQICS_Api
 
             services.AddControllers();
             services.AddDbContext<SQICSContext>(o => o.UseSqlServer(Configuration.GetConnectionString("SQICSConnection")));
+            services.AddScoped<ITransactionRepository, TransactionRepository>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SQICS_Api", Version = "v1" });
