8f93c3a9580bd0f6c8bdc83e5e18cd4f2cfce254
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-18 15:02:57 +0800

    addeed addplanDTO

---
+++ SQICS_Api/Model/AddPlanDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Model
+{
+    public class AddPlanDTO
+    {
+        public string SubAssyCode { get; set; }
+        public int SupplierId { get; set; }
+
+        public int LineId { get; set; }
+
+        public int TransactionNo { get; set; }
+
+        public int AssyId { get; set; }
+
+        public int? Qty { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime ETimeCompletion { get; set; }
+    }
+}
