7efe941183c2d37e3b483aa122f5b91a47abd626
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-18 11:46:56 +0800

    added plan service

---
+++ SQICS_Api/Service/Interface/IPlanService.cs
@@ -0,0 +1,17 @@
+﻿using SQICS_Api.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Service.Interface
+{
+    public interface IPlanService
+    {
+        Task<PlanDTO> GetPlanByTransactionNoAsync(int transNo);
+
+        Task<List<PlanDTO>> GetAllPlanAsync();
+
+        Task AddNewPlanAsync();
+    }
+}
---
+++ SQICS_Api/Service/PlanService.cs
@@ -0,0 +1,49 @@
+﻿using SQICS_Api.DTOs;
+using SQICS_Api.Service.Interface;
+using SQICS_Api.UOW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Service
+{
+    public class PlanService : IPlanService
+    {
+        IUnitOfWork _uow;
+
+        public PlanService(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+        public Task AddNewPlanAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<PlanDTO>> GetAllPlanAsync()
+        {
+            var transactions = await _uow.Transaction.GetAllTransactionAsync();
+            var subAssies = await _uow.SubAssy.GetAllSubAssyAsync();
+
+            var result = (from t in transactions
+                         join s in subAssies on t.fld_assyId equals s.fld_id
+                         select new PlanDTO
+                         {
+                             Id = t.fld_id,
+                             TransactionNo = t.fld_transactionNo,
+                             SubAssyCode = s.fld_partCode,
+                             SubAssyName = s.fld_partName,
+                             Qty = t.fld_qty,
+                             ETimeCompletion = t.ETimeCompletion
+                         }).ToList();
+
+            return result;
+        }
+
+        public Task<PlanDTO> GetPlanByTransactionNoAsync(int transNo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
