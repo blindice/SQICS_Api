5788637238a8ff5d7a27fa2c891d58274175df19
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-18 10:08:08 +0800

    added GetAllSubAssy SP

---
+++ SQICS_Api/Migrations/20220718015909_AddGetAllAssySP.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SQICS_Api.Migrations
+{
+    public partial class AddGetAllAssySP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE PROCEDURE [dbo].[usp_GetAllSubAssy]
+
+AS
+BEGIN
+
+    SELECT [fld_id]
+          ,[fld_supplierId]
+          ,[fld_partCode]
+          ,[fld_partName]
+    FROM [dbo].[tbl_m_parts] WITH(NOLOCK)
+    WHERE [fld_isAssy] = 1
+    AND [fld_active] = 1
+
+END";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
