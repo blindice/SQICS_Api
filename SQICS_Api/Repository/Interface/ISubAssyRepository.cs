b0335ce6d1da7ac81b23b41325f075ca7fd341b5
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-18 10:08:20 +0800

    added subassy repo with get all func

---
+++ SQICS_Api/Repository/Interface/ISubAssyRepository.cs
@@ -0,0 +1,14 @@
+﻿using SQICS_Api.Model;
+using SQICS_Api.Repository.Base.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.Interface
+{
+    public interface ISubAssyRepository : IRepositoryBase<tbl_m_part>
+    {
+        Task<IEnumerable<tbl_m_part>> GetAllSubAssyAsync();
+    }
+}
---
+++ SQICS_Api/Repository/SubAssy/SubAssyRepository.cs
@@ -0,0 +1,22 @@
+﻿using SQICS_Api.Model;
+using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Base;
+using SQICS_Api.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.SubAssy
+{
+    public class SubAssyRepository : RepositoryBase<tbl_m_part>, ISubAssyRepository
+    {
+
+        public SubAssyRepository(SQICSContext context) : base(context){}
+
+        public async Task<IEnumerable<tbl_m_part>> GetAllSubAssyAsync()
+        {
+            return await QueryAsync("usp_GetAllSubAssy");
+        }
+    }
+}
