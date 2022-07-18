c25f678a95f40618f7ea78fa7064e84629cae190
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-18 11:46:19 +0800

    added dappercontext

---
+++ SQICS_Api/Model/Context/DapperContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Model.Context
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("SQICSConnection");
+        }
+        public IDbConnection Connection { get => new SqlConnection(_connectionString); }
+    }
+}
