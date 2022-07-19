ba9ae7ddac50a127b3c337b1edfd0a6bb812b291
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-19 10:14:24 +0800

    added mapping in subassyProfile

--- SQICS_Api/Profiles/SubAssy/SubAssyProfile.cs
+++ SQICS_Api/Profiles/SubAssy/SubAssyProfile.cs
@@ -16,7 +16,11 @@ namespace SQICS_Api.Profiles.SubAssy
                 .ForMember(dest => dest.SubAssyCode,
                 opt => opt.MapFrom(src => src.fld_partCode))
                  .ForMember(dest => dest.SubAssyName,
-                opt => opt.MapFrom(src => src.fld_partName)).ReverseMap();
+                opt => opt.MapFrom(src => src.fld_partName))
+                  .ForMember(dest => dest.MOST,
+                opt => opt.MapFrom(src => src.fld_most))
+                   .ForMember(dest => dest.SPQ,
+                opt => opt.MapFrom(src => src.fld_spq)).ReverseMap();
 
             CreateMap<AddPlanDTO, tbl_t_transaction>();
         }
