606b8c5cea57048569d7dc82d39a30e4f88d0705
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-20 15:14:27 +0800

    added login controller

---
+++ SQICS_Api/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQICS_Api.Model;
+using SQICS_Api.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Controllers
+{
+    [Route("api/v1.0/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginService _service;
+        public LoginController(ILoginService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] LoginDTO account)
+        {
+            if (account is null) return BadRequest("Invalid Account!");
+
+            if (!ModelState.IsValid) return BadRequest("Invalid Account");
+
+            var result = await _service.VerifyUserAsync(account);
+
+            if (result is null) return NotFound("Account Not Found!");
+
+            return Ok(result);
+        }
+    }
+}
