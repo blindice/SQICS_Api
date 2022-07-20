baff142a5b8d5b9efc7991925aa88b0668fe67fc
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-20 15:14:20 +0800

    added login svc

---
+++ SQICS_Api/Service/Login/LoginService.cs
@@ -0,0 +1,24 @@
+﻿using SQICS_Api.Model;
+using SQICS_Api.Repository.Interface;
+using SQICS_Api.Service.Interface;
+using SQICS_Api.UOW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Service.Login
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IUnitOfWork _uow;
+        public LoginService(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+        public Task<UserInfoDTO> VerifyUserAsync(LoginDTO account)
+        {
+            throw new Exception();
+        }
+    }
+}
