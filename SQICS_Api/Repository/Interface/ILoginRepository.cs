4eab80d1940fadbccb6ad3feb0cb2ce3c457f4de
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-20 15:14:00 +0800

    added login repo

---
+++ SQICS_Api/Repository/Interface/ILoginRepository.cs
@@ -0,0 +1,13 @@
+﻿using SQICS_Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.Interface
+{
+    public interface ILoginRepository
+    {
+        Task<UserInfoDTO> VerifyUser(LoginDTO account);
+    }
+}
---
+++ SQICS_Api/Repository/Login/LoginRepository.cs
@@ -0,0 +1,22 @@
+﻿using SQICS_Api.Model;
+using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Base;
+using SQICS_Api.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.Login
+{
+    public class LoginRepository : RepositoryBase<UserInfoDTO>, ILoginRepository
+    {
+        public LoginRepository(SQICSContext efContext, DapperContext dapperContext)
+            : base(efContext, dapperContext) { }
+
+        public Task<UserInfoDTO> VerifyUser(LoginDTO account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
