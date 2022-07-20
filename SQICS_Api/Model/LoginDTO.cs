bb0d49132c95e43d5b45c89ba293549fedc99a0e
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-20 15:13:43 +0800

    added loginDTO

---
+++ SQICS_Api/Model/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Model
+{
+    public class LoginDTO
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
