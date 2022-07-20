d5f51c6031e4924738f92d344631b99a47250775
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-20 15:13:49 +0800

    added USerInfoDTO

---
+++ SQICS_Api/Model/UserInfoDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Model
+{
+    public class UserInfoDTO
+    {
+        public string Username { get; set; }
+
+        public string name { get; set; }
+
+        public int SupplierId { get; set; }
+
+        public string Suppliername { get; set; }
+    }
+}
