e2d60886e73c117c9b79370ad9194194dce9e16a
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-20 09:15:15 +0800

    added customexception

---
+++ SQICS_Api/Helper/CustomException/CustomException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Helper.CustomException
+{
+    public class CustomException : Exception
+    {
+        public CustomException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
