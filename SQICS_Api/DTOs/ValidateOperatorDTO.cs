7e6319bf020df98da6329cd34b88e4712b299b76
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-25 15:27:18 +0800

    added validate operator and piecepart DTO

---
+++ SQICS_Api/DTOs/ValidateOperatorDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class ValidateOperatorDTO
+    {
+        [JsonIgnore]
+        public int? OperatorId { get; set; }
+        [Required]
+        public string EmployeeId { get; set; }
+        [Required]
+        public int? SubAssyId { get; set; }
+        [Required]
+        public int? StationId { get; set; }
+    }
+}
---
+++ SQICS_Api/DTOs/ValidatePiecePartDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class ValidatePiecePartDTO
+    {
+        [Required]
+        public string PiecePartCOde { get; set; }
+        [JsonIgnore]
+        public int? PiecePartId { get; set; }
+
+        [Required]
+        public int? SubAssyId { get; set; }
+    }
+}
