591c10412597e05e2d3a1b7d72bbfb62df8bef9e
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-25 10:08:16 +0800

    added operator repo

---
+++ SQICS_Api/Repository/Operator/OperatorRepository.cs
@@ -0,0 +1,24 @@
+﻿using SQICS_Api.Model;
+using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Base;
+using SQICS_Api.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.Operator
+{
+    public class OperatorRepository : RepositoryBase<tbl_m_operator>, IOperatorRepository
+    {
+        public OperatorRepository(SQICSContext efContext, DapperContext dapperContext)
+           : base(efContext, dapperContext) { }
+
+        public async Task<tbl_m_operator> GetOperatorByEmpId(string empId)
+        {
+            var result = await QuerySingleOrDefaultAsync("usp_GetOperatorByEmpId", new { empId = empId});
+
+            return result;
+        }
+    }
+}
