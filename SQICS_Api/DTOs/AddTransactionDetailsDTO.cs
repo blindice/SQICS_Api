ba07c07bfe9febf74238e297cd390b4cc5a89a7f
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-26 06:58:31 +0800

    added AddTransactionDetailsDTO

---
+++ SQICS_Api/DTOs/AddTransactionDetailsDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class AddTransactionDetailsDTO
+    {
+        [Required]
+        public string TransactionNo { get; set; }
+        [Required]
+        public int StationId { get; set; }
+        [Required]
+        public string EmployeeId { get; set; }
+        [Required]
+        public string PiecePartId { get; set; }
+        [Required]
+        public string LotNo { get; set; }
+        [Required]
+        public string RefNo { get; set; }
+        [Required]
+        public int Qty { get; set; }
+    }
+}
