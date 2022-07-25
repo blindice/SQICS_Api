8f03d5b3ff4f7b4cc6e34cb00bd32e4ad6237bdc
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-25 09:36:31 +0800

    added GetTransactionDetailsByTransNoAsync in transaction svc

--- SQICS_Api/Service/Interface/ITransactionService.cs
+++ SQICS_Api/Service/Interface/ITransactionService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQICS_Api.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace SQICS_Api.Service.Interface
 {
     public interface ITransactionService
     {
+        Task<List<TransactionDetailsDTO>> GetTransactionDetailsByTransNoAsync(string transNo);
     }
 }
--- SQICS_Api/Service/Transaction/TransactionService.cs
+++ SQICS_Api/Service/Transaction/TransactionService.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using SQICS_Api.Helper.CustomException;
+using SQICS_Api.Model;
+using SQICS_Api.Service.Interface;
+using SQICS_Api.UOW;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SQICS_Api.Service.Transaction
 {
-    public class TransactionService
+    public class TransactionService : ITransactionService
     {
+        IUnitOfWork _uow;
+
+        public TransactionService(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public async Task<List<TransactionDetailsDTO>> GetTransactionDetailsByTransNoAsync(string transNo)
+        {
+            var result = await _uow.TDetails.GetTransactionDetailsByTransNoAsync(transNo);
+
+            if (result is null) throw new CustomException("Invalid Transaction Details!");
+
+            return result.ToList();
+        }
     }
 }
