ab2201713bacd01b3aaff86f208591e95e5adb77
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-25 15:28:26 +0800

    added validation in piecepart repo

---
+++ SQICS_Api/Repository/Interface/IPiecePartRepository.cs
@@ -0,0 +1,15 @@
+﻿using SQICS_Api.DTOs;
+using SQICS_Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.Interface
+{
+    public interface IPiecePartRepository
+    {
+        Task<tbl_m_part> GetPiecePartByCode(string pieceCode);
+        Task<bool> ValidatePiecePart(ValidatePiecePartDTO info);
+    }
+}
---
+++ SQICS_Api/Repository/PiecePart/PiecePartRepository.cs
@@ -0,0 +1,38 @@
+﻿using SQICS_Api.DTOs;
+using SQICS_Api.Model;
+using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Base;
+using SQICS_Api.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.PiecePart
+{
+    public class PiecePartRepository : RepositoryBase<tbl_m_part>, IPiecePartRepository
+    {
+        public PiecePartRepository(SQICSContext efContext, DapperContext dapperContext)
+         : base(efContext, dapperContext) { }
+
+        public async Task<tbl_m_part> GetPiecePartByCode(string pieceCode)
+        {
+            var result = await QuerySingleOrDefaultAsync("usp_GetPiecePartByCode", new { pieceCode = pieceCode });
+
+            return result;
+        }
+
+        public async Task<bool> ValidatePiecePart(ValidatePiecePartDTO info)
+        {
+            var parameters = new Dictionary<string, object>()
+            {
+                ["pieceId"] = info.PiecePartId,
+                ["assyId"] = info.SubAssyId
+            };
+
+            var isValid = await ExecuteScalarAsync<bool>("usp_ValidatePiecePart", parameters);
+
+            return isValid;
+        }
+    }
+}
