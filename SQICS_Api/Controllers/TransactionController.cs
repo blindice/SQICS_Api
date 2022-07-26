29550d099ad5aede5f5071a2df925424012255e8
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-26 08:50:00 +0800

    added ProducesResponseType in transaction controller

--- SQICS_Api/Controllers/TransactionController.cs
+++ SQICS_Api/Controllers/TransactionController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SQICS_Api.DTOs;
+using SQICS_Api.Model;
 using SQICS_Api.Repository.Interface;
 using SQICS_Api.Service.Interface;
 using SQICS_Api.UOW;
@@ -22,6 +24,10 @@ namespace SQICS_Api.Controllers
         }
 
         [HttpGet("{transNo}")]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(List<AddTransactionDetailsDTO>), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetTransactionDetailsByTransNoAsync(string transNo)
         {
             if (string.IsNullOrEmpty(transNo)) return BadRequest("Invalid Transaction No.!");
@@ -30,19 +36,13 @@ namespace SQICS_Api.Controllers
 
             return Ok(details);
         }
-
-        [HttpPost("validateoperator")]
-        public async Task<IActionResult> ValidateOperatorAsync([FromBody] ValidateOperatorDTO info)
-        {
-            if (!ModelState.IsValid || info is null) 
-                return BadRequest("Invalid Validation Info!");
-
-            var isValid = await _service.ValidateOperatorAsync(info);
-
-            return (isValid) ? Ok() : NotFound("Invalid Operator!");
-        }
+       
 
         [HttpPost("validatepiece")]
+        [AllowAnonymous]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> ValidatePiecePartAsync([FromBody] ValidatePiecePartDTO info)
         {
             if (!ModelState.IsValid || info is null) 
@@ -54,10 +54,16 @@ namespace SQICS_Api.Controllers
         }
 
         [HttpPost("add")]
+        [AllowAnonymous]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> AddTransactionDetailsAsync([FromBody] AddTransactionDetailsDTO transDetails)
         {
             if (!ModelState.IsValid || transDetails is null) 
                 return BadRequest("Invalid Transaction Details!");
+
+            await _service.AddTransactionDetailsAsync(transDetails);
 
             return Ok();
         }
