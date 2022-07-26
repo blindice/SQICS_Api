466e7b6aef2f88d32d801e047ac91b317ee7d95b
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-26 14:26:22 +0800

    added value cant be zero model validation

--- SQICS_Api/DTOs/AddPlanDTO.cs
+++ SQICS_Api/DTOs/AddPlanDTO.cs
@@ -10,12 +10,16 @@ namespace SQICS_Api.DTOs
     public class AddPlanDTO
     {
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Supplier ID can't be zero!")]
         public int? fld_supplierId { get; set; }
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Line ID can't be zero!")]
         public int? fld_lineId { get; set; }
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "SubAssy ID can't be zero!")]
         public int? fld_assyId { get; set; }
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Qty can't be zero!")]
         public int? fld_qty { get; set; }
         [Required]
         public DateTime? ETimeCompletion { get; set; }
