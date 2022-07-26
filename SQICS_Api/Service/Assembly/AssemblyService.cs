6361d991bfa9b7c2b07b61178b0c995b2b005dba
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-26 08:49:19 +0800

    added validate operator in assembly service

---
+++ SQICS_Api/Service/Assembly/AssemblyService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using SQICS_Api.DTOs;
+using SQICS_Api.Helper.CustomException;
+using SQICS_Api.Service.Interface;
+using SQICS_Api.UOW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Service.Assembly
+{
+    public class AssemblyService : IAssemblyService
+    {
+        IUnitOfWork _uow;
+        IMapper _mapper;
+
+        public AssemblyService(IUnitOfWork uow, IMapper mapper)
+        {
+            _uow = uow;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> ValidateOperatorAsync(ValidateOperatorDTO info)
+        {
+            var @operator = await _uow.Operator.GetOperatorByEmpId(info.EmployeeId);
+
+            if (@operator is null) throw new CustomException("Operator Not Found!");
+
+            info.OperatorId = @operator.fld_id;
+
+            var isValid = await _uow.Operator.ValidateOperator(info);
+
+            return isValid;
+        }
+    }
+}
---
+++ SQICS_Api/Service/Interface/IAssemblyService.cs
@@ -0,0 +1,13 @@
+﻿using SQICS_Api.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Service.Interface
+{
+    public interface IAssemblyService
+    {
+        Task<bool> ValidateOperatorAsync(ValidateOperatorDTO info);
+    }
+}
