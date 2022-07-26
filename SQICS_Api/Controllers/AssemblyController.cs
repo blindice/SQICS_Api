0d1310feebede7463b23cf67f7b9894c8ca9ce72
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-26 08:51:11 +0800

    added validate operator in assembly controller

--- SQICS_Api/Controllers/AssemblyController.cs
+++ SQICS_Api/Controllers/AssemblyController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SQICS_Api.DTOs;
+using SQICS_Api.Model;
+using SQICS_Api.Service.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +11,29 @@ using System.Threading.Tasks;
 
 namespace SQICS_Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1.0/[controller]")]
     [ApiController]
     public class AssemblyController : ControllerBase
     {
+        IAssemblyService _service;
+        public AssemblyController(IAssemblyService service)
+        {
+            _service = service;
+        }
 
+        [HttpPost("validateoperator")]
+        [AllowAnonymous]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> ValidateOperatorAsync([FromBody] ValidateOperatorDTO info)
+        {
+            if (!ModelState.IsValid || info is null)
+                return BadRequest("Invalid Validation Info!");
+
+            var isValid = await _service.ValidateOperatorAsync(info);
+
+            return (isValid) ? Ok() : NotFound("Invalid Operator!");
+        }
     }
 }
