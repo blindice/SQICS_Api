0224585ad43ab57b0c36c65735b58058cf9241e8
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-26 15:41:13 +0800

    added defect repo

---
+++ SQICS_Api/Repository/Defect/DefectRepository.cs
@@ -0,0 +1,14 @@
+﻿using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.Defect
+{
+    public class DefectRepository : IDefectRepository
+    {
+
+    }
+}
