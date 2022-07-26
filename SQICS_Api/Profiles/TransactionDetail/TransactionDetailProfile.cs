33b520e617e98449dfa8cd74ae4cce5a138966e5
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-26 08:50:12 +0800

    added transactionDetail Profile

---
+++ SQICS_Api/Profiles/TransactionDetail/TransactionDetailProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SQICS_Api.DTOs;
+using SQICS_Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Profiles.TransactionDetail
+{
+    public class TransactionDetailProfile : Profile
+    {
+        public TransactionDetailProfile()
+        {
+            CreateMap<AddTransactionDetailsDTO, tbl_t_transaction_detail>();
+        }
+    }
+}
