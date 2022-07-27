637955bd512962a0686111ad8430e465f9419154
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-07-27 08:39:10 +0800

    added getallDefect SP

---
+++ SQICS_Api/Migrations/20220727001624_AddGetAllDefects_SP.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SQICS_Api.Migrations
+{
+    public partial class AddGetAllDefects_SP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Ludz
+-- Create date: 07-27-22
+-- Description:	
+-- =============================================
+CREATE PROCEDURE usp_GetAllDefects 
+	-- Add the parameters for the stored procedure here
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT [fld_id]
+		  ,[fld_name]
+    FROM   [db_SQICS].[dbo].[tbl_m_defects]
+	WHERE  [fld_active] = 1
+
+END
+GO";
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
