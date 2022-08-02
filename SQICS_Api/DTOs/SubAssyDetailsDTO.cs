adc4dc2f7e5693de822893f9f226912d3bee04a1
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-02 10:40:02 +0800

    added subassydetailsDTO

---
+++ SQICS_Api/DTOs/SubAssyDetailsDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class SubAssyDetailsDTO
+    {
+
+        public string fld_partName { get; set; }
+
+        public string fld_partCode { get; set; }
+
+        public int fld_spq { get; set; }
+
+        public int fld_most { get; set; }
+    }
+}
