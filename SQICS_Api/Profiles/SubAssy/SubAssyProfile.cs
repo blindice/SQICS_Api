ce2ba499f4d15aa080978ba1520e8d793208aff4
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-02 08:46:50 +0800

    changed subassy profile

--- SQICS_Api/Profiles/SubAssy/SubAssyProfile.cs
+++ SQICS_Api/Profiles/SubAssy/SubAssyProfile.cs
@@ -12,17 +12,7 @@ namespace SQICS_Api.Profiles.SubAssy
     {
         public SubAssyProfile()
         {
-            CreateMap<tbl_m_part, SubAssyDDLDTO>()
-                .ForMember(dest => dest.SubAssyId,
-                opt => opt.MapFrom(src => src.fld_id))
-                .ForMember(dest => dest.SubAssyCode,
-                opt => opt.MapFrom(src => src.fld_partCode))
-                 .ForMember(dest => dest.SubAssyName,
-                opt => opt.MapFrom(src => src.fld_partName))
-                  .ForMember(dest => dest.MOST,
-                opt => opt.MapFrom(src => src.fld_most))
-                   .ForMember(dest => dest.SPQ,
-                opt => opt.MapFrom(src => src.fld_spq)).ReverseMap();
+            CreateMap<tbl_m_part, SubAssyDDLDTO>().ReverseMap();
 
             CreateMap<AddPlanDTO, tbl_t_transaction>();
         }
