4d2c2755061bc9bcd63e6a96e0852663c171bbb2
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-03 15:36:50 +0800

    added CurrentPlanDTO

---
+++ SQICS_Api/DTOs/CurrentPlanDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class CurrentPlanDTO
+    {
+        public string LotNo { get; set; }
+
+        public int Qty { get; set; }
+
+        public string PartCode { get; set; }
+
+        public string PartName { get; set; }
+
+        public string Status { get; set; }
+    }
+}
