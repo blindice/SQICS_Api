9844ab8c9e84939237e954c8e5609c3ea9188912
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-04 10:56:11 +0800

    added shift Enum

---
+++ SQICS_Api/Enum/Shift.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Enum
+{
+    public enum Shift
+    {
+        Day = 1,
+        Night = 2
+    }
+}
