ca90e5da2431feab5f22de191b6fee842097d46d
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-04 15:24:42 +0800

    added StationDDLDTO

---
+++ SQICS_Api/DTOs/StationDDLDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class StationDDLDTO
+    {
+        public int Id { get; set; }
+
+        public string StationName { get; set; }
+    }
+}
