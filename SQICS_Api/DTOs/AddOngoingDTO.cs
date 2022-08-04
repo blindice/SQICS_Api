1a0cf1f7d12fca18622c571b2433e7a2fabd33e7
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-04 10:55:44 +0800

    added AddOngoingDTO

---
+++ SQICS_Api/DTOs/AddOngoingDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class AddOngoingDTO
+    {
+        [Required]
+        public int TransId { get; set; }
+        [Required]
+        public int? fld_lineId { get; set; }
+        [Required]
+        public int? fld_stationId { get; set; }
+        [Required]
+        public string fld_trans { get; set; }
+        [Required]
+        public string fld_lotNo { get; set; }
+        [Required]
+        public string fld_partCode { get; set; }
+        [Required]
+        public int? fld_qty { get; set; }
+    }
+}
