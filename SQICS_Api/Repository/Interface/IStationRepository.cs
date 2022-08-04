ac944853c13e3730a367953592278df08ed8a896
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-04 15:25:51 +0800

    added station repo

---
+++ SQICS_Api/Repository/Interface/IStationRepository.cs
@@ -0,0 +1,13 @@
+﻿using SQICS_Api.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.Interface
+{
+    public interface IStationRepository
+    {
+        Task<StationDDLDTO> GetStationDDLBySupplierIdAsync(int supplierId);
+    }
+}
---
+++ SQICS_Api/Repository/StationRepository.cs
@@ -0,0 +1,13 @@
+﻿using SQICS_Api.Repository.Base;
+using SQICS_Api.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository
+{
+    public class StationRepository /* RepositoryBase<tbl_m_station>, IStationRepository*/
+    {
+    }
+}
