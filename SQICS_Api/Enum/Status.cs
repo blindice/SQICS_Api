1b6fe30e4a9e936bf675beb49765824e6ca80606
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-04 13:50:52 +0800

    added Status Enum

---
+++ SQICS_Api/Enum/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Enum
+{
+    public enum Status
+    {
+        NotYetStarted = 0,
+        Started = 1,
+        OnGoing = 2,
+        Deleted = 3,
+        Finish = 4
+    }
+}
