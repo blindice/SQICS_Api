360136b180cac009602b33392a48b856bb71b6e7
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-04 15:38:06 +0800

    added contrunctor in line repo

--- SQICS_Api/Repository/Line/LineRepository.cs
+++ SQICS_Api/Repository/Line/LineRepository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using SQICS_Api.Model;
+using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Base;
+using SQICS_Api.Repository.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SQICS_Api.Repository.Line
 {
-    public class LineRepository
+    public class LineRepository : RepositoryBase<tbl_m_line>, ILineRepository
     {
+        public LineRepository(SQICSContext efContext, DapperContext dapperContext)
+            : base(efContext, dapperContext) { }
     }
 }
