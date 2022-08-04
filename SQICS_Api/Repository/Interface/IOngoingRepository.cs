40867f2842ecf1ad8837b026461fbca8948b2f1e
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-04 09:10:05 +0800

    added Ongoing repo with add func

---
+++ SQICS_Api/Repository/Interface/IOngoingRepository.cs
@@ -0,0 +1,13 @@
+﻿using SQICS_Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.Interface
+{
+    public interface IOngoingRepository
+    {
+        Task AddOngoingLot(tbl_t_lot_ongoing lot);
+    }
+}
---
+++ SQICS_Api/Repository/Ongoing/OngoingRepository.cs
@@ -0,0 +1,22 @@
+﻿using SQICS_Api.Model;
+using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Base;
+using SQICS_Api.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.Ongoing
+{
+    public class OngoingRepository: RepositoryBase<tbl_t_lot_ongoing>, IOngoingRepository
+    {
+        public OngoingRepository(SQICSContext efContext, DapperContext dapperContext)
+          : base(efContext, dapperContext) { }
+
+        public async Task AddOngoingLot(tbl_t_lot_ongoing lot)
+        {
+            await AddAsync(lot);
+        }
+    }
+}
