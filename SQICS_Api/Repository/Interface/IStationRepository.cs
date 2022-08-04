cf36b93a66df5fb3b7ef15ba1f4ae5b0544c9995
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-04 15:34:00 +0800

    added GetStationDDLBySupplierIdAsync in station repo

--- SQICS_Api/Repository/Interface/IStationRepository.cs
+++ SQICS_Api/Repository/Interface/IStationRepository.cs
@@ -8,6 +8,6 @@ namespace SQICS_Api.Repository.Interface
 {
     public interface IStationRepository
     {
-        Task<StationDDLDTO> GetStationDDLBySupplierIdAsync(int supplierId);
+        Task<IEnumerable<StationDDLDTO>> GetStationDDLBySupplierIdAsync(int supplierId);
     }
 }
--- SQICS_Api/Repository/StationRepository.cs
+++ SQICS_Api/Repository/StationRepository.cs
@@ -1,4 +1,7 @@
-﻿using SQICS_Api.Repository.Base;
+﻿using SQICS_Api.DTOs;
+using SQICS_Api.Model;
+using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Base;
 using SQICS_Api.Repository.Interface;
 using System;
 using System.Collections.Generic;
@@ -7,7 +10,15 @@ using System.Threading.Tasks;
 
 namespace SQICS_Api.Repository
 {
-    public class StationRepository /* RepositoryBase<tbl_m_station>, IStationRepository*/
+    public class StationRepository : RepositoryBase<tbl_m_station>, IStationRepository
     {
+        public StationRepository(SQICSContext efContext, DapperContext dapperContext)
+    : base(efContext, dapperContext) { }
+        public async Task<IEnumerable<StationDDLDTO>> GetStationDDLBySupplierIdAsync(int supplierId)
+        {
+            var stations = await QueryAsync<StationDDLDTO>("usp_GetStationDDL", new { supplierId = supplierId });
+
+            return stations;
+        }
     }
 }
