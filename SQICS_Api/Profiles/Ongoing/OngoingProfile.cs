c611469e956ecf3d74f1f0022af791dc46d6ba48
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-04 10:56:05 +0800

    added new ongoing mapping profile

---
+++ SQICS_Api/Profiles/Ongoing/OngoingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SQICS_Api.DTOs;
+using SQICS_Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Profiles.Ongoing
+{
+    public class OngoingProfile : Profile
+    {
+        public OngoingProfile()
+        {
+            CreateMap<AddOngoingDTO, tbl_t_lot_ongoing>().ReverseMap();
+        }
+    }
+}
