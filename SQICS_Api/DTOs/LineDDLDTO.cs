4b4eac0d0d21f2dccc5b66850870b8dee2e16cb9
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-04 15:38:50 +0800

    added ILineRepository

---
+++ SQICS_Api/DTOs/LineDDLDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class LineDDLDTO
+    {
+        public int Id { get; set; }
+
+        public string LineName { get; set; }
+    }
+}
