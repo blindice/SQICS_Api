ae9fd71bb7ff483437652287375b4700bb78928c
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-09 09:07:37 +0800

    added AddHeaderAsync in transHeader repo

--- SQICS_Api/Repository/TransactionHeader/TransactionHeaderRepository.cs
+++ SQICS_Api/Repository/TransactionHeader/TransactionHeaderRepository.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using SQICS_Api.Model;
+using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Base;
+using SQICS_Api.Repository.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SQICS_Api.Repository.TransactionHeader
 {
-    public class TransactionHeaderRepository
+    public class TransactionHeaderRepository : RepositoryBase<tbl_t_transaction_header>, ITransactionHeaderRepository
     {
+        public TransactionHeaderRepository(SQICSContext efContext, DapperContext dapperContext)
+             : base(efContext, dapperContext) { }
+
+        public async Task AddHeaderAsync(tbl_t_transaction_header header)
+        {
+            await AddAsync(header);
+        }
     }
 }
