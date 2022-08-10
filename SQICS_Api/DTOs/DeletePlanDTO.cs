fe1450775793687864a60a4bc0096b7486fa8bc3
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-10 08:50:57 +0800

    added DeletePlanDTO

---
+++ SQICS_Api/DTOs/DeletePlanDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class DeletePlanDTO
+    {
+        public string TransNo { get; set; }
+
+        public int OperatorId { get; set; }
+    }
+}
