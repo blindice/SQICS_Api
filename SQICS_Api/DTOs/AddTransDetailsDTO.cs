72a36f311073ffc101c1f13cd751ee95ae57f3cf
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-10 14:48:29 +0800

    added AddTransDetailsDTO

---
+++ SQICS_Api/DTOs/AddTransDetailsDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class AddTransDetailsDTO
+    {
+        public int? TransId { get; set; }
+
+        public int? StationId { get; set; }
+
+        public int? EmployeeId { get; set; }
+
+        public int? PieceId { get; set; }
+
+        public string LotNo { get; set; }
+
+        public string RefNo { get; set; }
+
+        public int? Qty { get; set; }
+
+        public int? CreatedBy { get; set; }
+    }
+}
