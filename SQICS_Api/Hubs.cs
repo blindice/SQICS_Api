50da67312efea916fcfffc2211789beee2753fea
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-10 14:47:55 +0800

    added signalR hub

---
+++ SQICS_Api/Hubs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api
+{
+    public class Hubs : Hub
+    {
+    }
+}
