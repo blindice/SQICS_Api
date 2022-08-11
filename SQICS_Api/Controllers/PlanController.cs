ccbd8b553dc82cf35c29bb2cc950436a4e8e6bea
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-11 08:45:56 +0800

    assembly to plan controller

--- SQICS_Api/Controllers/PlanController.cs
+++ SQICS_Api/Controllers/PlanController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using SQICS_Api.DTOs;
 using SQICS_Api.Helper.POCO;
 using SQICS_Api.Model;
@@ -16,72 +17,171 @@ namespace SQICS_Api.Controllers
     [ApiController]
     public class PlanController : ControllerBase
     {
-        private readonly IPlanService _service;
+        IAssemblyService _service;
+        IHubContext<Hubs> _hub;
 
-        public PlanController(IPlanService service) => _service = service;
+        public PlanController(IAssemblyService service, IHubContext<Hubs> hub)
+        {
+            _service = service;
+            _hub = hub;
+        }
 
-        [HttpGet("plans")]
+        [HttpGet("operatordetails/{operatorId}")]
         [AllowAnonymous]
-        [ProducesResponseType(typeof(List<PlanDTO>), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(SubAssyByOperatorIdDTO), statusCode: StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
         [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(statusCode: StatusCodes.Status401Unauthorized)]
-        public async Task<IActionResult> GetAllPlanAsync()
+        public async Task<IActionResult> GetOperatorDetailsAsync(string operatorId)
         {
-            var plans = await _service.GetAllPlanAsync();
+            if (!ModelState.IsValid || operatorId is null)
+                return BadRequest("Invalid Operator Id!");
 
-            if (plans is null) return NotFound("No Plan Found!");
+            var operatorDetails = await _service.GetOperatorDetailsAsync(operatorId);
+
+            return Ok(operatorDetails);
+        }
+
+        [HttpGet("subassies/{supplierId:int}")]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(SubAssyDDLDTO), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetDDLSubassiesAsync(int? supplierId)
+        {
+            if (supplierId is null) return BadRequest("Invalid Supplier Id!");
+
+            var subAssies = await _service.GetSubAssyDdlDataAsync((int)supplierId);
+
+            return Ok(subAssies);
+        }
+
+        [HttpGet("subassybycode")]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(SubAssyDetailsDTO), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetSubAssyByCodeAsync([FromQuery] int? supplierId, [FromQuery] string subAssyCode)
+        {
+            if (supplierId is null || subAssyCode is null)
+                return BadRequest("Invalid Query Strings!");
+
+            var subAssy = await _service.GetSubAssyByCodeAsync((int)supplierId, subAssyCode);
+
+            return Ok(subAssy);
+        }
+
+
+        [HttpGet("subassybyname")]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(SubAssyDetailsDTO), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetSubAssyByNameAsync([FromQuery] int? supplierId, [FromQuery] string subAssyName)
+        {
+            if (supplierId is null || subAssyName is null)
+                return BadRequest("Invalid Query Strings!");
+
+            var subAssy = await _service.GetSubAssyByNameAsync((int)supplierId, subAssyName);
+
+            return Ok(subAssy);
+        }
+
+        [HttpGet("servertime")]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(DateTime), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
+        public IActionResult GetServerSystemTime()
+        {
+            return Ok(DateTime.Now);
+        }
+
+        [HttpPost("addtransactions")]
+        [AllowAnonymous]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> AddTransactionsAsync([FromBody] List<AddPlanDTO> plans)
+        {
+            if (plans is null) return BadRequest("Invalid plans!");
+
+            if (plans.Count == 0) return NotFound("No Plan Found to Add!");
+
+            await _service.AddPlansAsync(plans);
+
+            return Ok();
+        }
+
+        [HttpGet("currentplans")]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(List<CurrentPlanDTO>), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetCurrentPlanAsync([FromQuery] int? lineId)
+        {
+            if (lineId is null) return BadRequest("Invalid Line Id!");
+
+            var plans = await _service.GetCurrentPlansByLineIdAsync((int)lineId);
 
             return Ok(plans);
         }
 
-        [HttpGet("search")]
+        [HttpPost("startprocess")]
         [AllowAnonymous]
-        [ProducesResponseType(typeof(List<PlanDTO>), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
         [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(statusCode: StatusCodes.Status401Unauthorized)]
-        public async Task<IActionResult> SearchPlanByFilterAsync([FromQuery] string value = "")
+        public async Task<IActionResult> StartProcessAsync([FromBody] AddOngoingDTO transaction)
         {
-            if (value is null) return BadRequest("Invalid Transaction No.!");
+            if (!ModelState.IsValid || transaction is null) return BadRequest("Invalid Transaction!");
 
-            var plans = await _service.GetPlanByFilters(value);
+            await _service.StartProcessAsync(transaction);
 
-            if (plans is null) return NotFound("Plan Not Found!");
-
-            return Ok(plans);
+            return Ok();
         }
 
-        [HttpPost("add")]
-        [Authorize]
-        [ProducesResponseType(typeof(AddPlanDTO), statusCode: StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(statusCode: StatusCodes.Status401Unauthorized)]
-        public async Task<IActionResult> AddNewPlan([FromBody] AddPlanDTO plan)
-        {
-            if (!ModelState.IsValid || plan is null) return BadRequest("Invalid Plan!");
-
-            await _service.AddNewPlanAsync(plan);
-
-            return Ok(plan);
-        }
-
-        [HttpGet("add")]
+        [HttpGet("ddldatas/{supplierId:int}")]
         [AllowAnonymous]
-        [ProducesResponseType(typeof(List<SubAssyDDLDTO>), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof((List<StationDDLDTO>, List<LineDDLDTO>)), statusCode: StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
         [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(statusCode: StatusCodes.Status401Unauthorized)]
-        public async Task<IActionResult> AddnewPlan()
+        public async Task<IActionResult> GetDDLDataAsync(int? supplierId)
         {
-            var subAssyDdl =  await _service.GetAllSubAssyDDLAsync();
+            if (supplierId is null) return BadRequest("Invalid Supplier Id!");
 
-            if (subAssyDdl is null) return NotFound("No SubAssy Found!");
+            var result = await _service.GetDDLDataAsync((int)supplierId);
 
-            return Ok(subAssyDdl);
+            return Ok(new { Stations = result.Item1, Lines = result.Item2 });
         }
 
+        [HttpPost("deleteplan")]
+        [AllowAnonymous]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> DeletePlan([FromBody] DeletePlanDTO field)
+        {
+            if (field is null) return BadRequest("Invalid Transaction!");
 
+            await _service.DeletePlanAsync(field);
+
+            return Ok();
+        }
+
+        [HttpPost("print")]
+        [AllowAnonymous]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorDetails), statusCode: StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Print([FromBody] PrintDTO info)
+        {
+            if (info is null) return BadRequest("Invalid Line Id!");
+
+            var plans = await _service.GetCurrentPlansByLineIdAsync((int)info.LineId);
+
+            await _hub.Clients.Group($"LineId-{info.LineId}").SendAsync("Print", plans);
+
+            return Ok();
+        }
     }
 }
