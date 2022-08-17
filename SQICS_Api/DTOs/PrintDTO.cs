e631e9d411eb09fe98c4bc919adc37c2b7dd3351
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-17 09:56:07 +0800

    added printDTO

--- SQICS_Api/DTOs/PrintDTO.cs
+++ SQICS_Api/DTOs/PrintDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,13 @@ namespace SQICS_Api.DTOs
 {
     public class PrintDTO
     {
+        [Required]
         public string LotNo { get; set; }
 
+        [Required]
         public int? OperatorId { get; set; }
 
+        [Required]
         public int? LineId { get; set; }
     }
 }
