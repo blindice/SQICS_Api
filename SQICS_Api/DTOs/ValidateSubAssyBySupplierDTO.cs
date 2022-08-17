61769f9a3f308f614e2c047fe316ba62f50cea43
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-17 13:27:32 +0800

    added ValidateSubAssyBySupplierDTO

---
+++ SQICS_Api/DTOs/ValidateSubAssyBySupplierDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class ValidateSubAssyBySupplierDTO
+    {
+        public int SupplierId { get; set; }
+
+        public string PieceCode { get; set; }
+
+        public string AssyCode { get; set; }
+    }
+}
