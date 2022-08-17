3e1b5988a86ffc7d6a4002eca42a71c27e056cf1
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-17 14:00:45 +0800

    added AssyDefectProfile

---
+++ SQICS_Api/Profiles/AssyDefect/AssyDefectProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SQICS_Api.DTOs;
+using SQICS_Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Profiles.AssyDefect
+{
+    public class AssyDefectProfile : Profile
+    {
+        public AssyDefectProfile()
+        {
+            CreateMap<AddAssyDefectDTO, tbl_t_assy_defect>().ReverseMap();
+        }
+    }
+}
