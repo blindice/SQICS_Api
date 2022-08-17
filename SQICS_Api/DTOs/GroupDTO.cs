a4a2a8c02eea30a2b08756fbe6ffe0d46a6ddfb3
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-17 09:56:14 +0800

    added groupdto

---
+++ SQICS_Api/DTOs/GroupDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class GroupDTO
+    {
+        [Required]
+        public string ConnectionId { get; set; }
+
+        [Required]
+        public string GroupName { get; set; }
+    }
+}
