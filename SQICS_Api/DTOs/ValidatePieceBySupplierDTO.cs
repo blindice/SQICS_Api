fa2040d2ed37b9e0bb9059553c4b911d9c432ced
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-17 11:09:06 +0800

    added ValidatePieceBySupplierDTO

---
+++ SQICS_Api/DTOs/ValidatePieceBySupplierDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class ValidatePieceBySupplierDTO
+    {
+        public int SupplierId { get; set; }
+
+        public string PiecePartCode { get; set; }
+    }
+}
