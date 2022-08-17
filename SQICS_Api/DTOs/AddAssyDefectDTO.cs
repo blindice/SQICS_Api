85154bb07b20c43f15cd27edf6ed903bee4cedb2
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-17 13:59:37 +0800

    added AddAssyDefectDTO

---
+++ SQICS_Api/DTOs/AddAssyDefectDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class AddAssyDefectDTO
+    {
+        [Required]
+        public string fld_pieceCode { get; set; }
+        [Required]
+        public string fld_pieceLot { get; set; }
+        [Required]
+        public string fld_assyCode { get; set; }
+        [Required]
+        public string fld_assyLot { get; set; }
+        [Required]
+        public int? fld_defectId { get; set; }
+        [Required]
+        public string fld_remarks { get; set; }
+        [Required]
+        public int? fld_qty { get; set; }
+        [Required]
+        public int? fld_createdBy { get; set; }
+    }
+}
