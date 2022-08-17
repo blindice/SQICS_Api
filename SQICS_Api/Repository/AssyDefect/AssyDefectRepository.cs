26a8e3fbe5ee864bd3192e61658ad689b8647eca
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-17 13:34:05 +0800

    added AssyDefectRepository

---
+++ SQICS_Api/Repository/AssyDefect/AssyDefectRepository.cs
@@ -0,0 +1,17 @@
+﻿using SQICS_Api.Model;
+using SQICS_Api.Model.Context;
+using SQICS_Api.Repository.Base;
+using SQICS_Api.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.AssyDefect
+{
+    public class AssyDefectRepository : RepositoryBase<tbl_t_assy_defect>, IAssyDefectRepository
+    {
+        public AssyDefectRepository(SQICSContext efContext, DapperContext dapperContext)
+       : base(efContext, dapperContext) { }
+    }
+}
