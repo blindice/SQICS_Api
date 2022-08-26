d7691735e399334087510d5adb813ae251f8f42c
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-26 11:26:38 +0800

    added ReprintController

---
+++ SQICS_Api/Controllers/ReprintController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Controllers
+{
+    [Route("api/v1.0/[controller]")]
+    [ApiController]
+    public class ReprintController : ControllerBase
+    {
+    }
+}
