57b44851ae45c1847419f1b4d3558fa0391a848e
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-08-26 09:19:32 +0800

    added lotlabel repo

---
+++ SQICS_Api/Repository/LotLabel/LotLabelRepository.cs
@@ -0,0 +1,12 @@
+﻿using SQICS_Api.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Repository.LotLabel
+{
+    public class LotLabelRepository : ILotLabelRepository
+    {
+    }
+}
