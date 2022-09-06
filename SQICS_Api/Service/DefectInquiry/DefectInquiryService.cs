deed2dd1303f009719d3ca863877d90d70c8c107
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-09-06 09:34:57 +0800

    added GetDefectInquiryAsync in defectinquiry svc

--- SQICS_Api/Service/DefectInquiry/DefectInquiryService.cs
+++ SQICS_Api/Service/DefectInquiry/DefectInquiryService.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using AutoMapper;
+using SQICS_Api.DTOs;
+using SQICS_Api.Helper.CustomException;
+using SQICS_Api.Service.Interface;
+using SQICS_Api.UOW;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SQICS_Api.Service.DefectInquiry
 {
-    public class DefectInquiryService
+    public class DefectInquiryService : IDefectInquiryService
     {
+        IUnitOfWork _uow;
+        IMapper _mapper;
+
+        public DefectInquiryService(IUnitOfWork uow, IMapper mapper)
+        {
+            _uow = uow;
+            _mapper = mapper;
+        }
+
+        public async Task<List<DefectInquiryDTO>> GetDefectInquiryAsync(DefectInquiryParams @params)
+        {
+            var defectInquiry = await _uow.AssyDefect.GetAssyDefectInquiryAsync(@params);
+
+            if (defectInquiry is null) throw new CustomException("Invalid Defect Inquiry!");
+
+            return defectInquiry.ToList();
+        }
     }
 }
