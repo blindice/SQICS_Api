8fee9c4f0e09318878537149c1b6a4dd5a1e5771
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-09-06 08:59:34 +0800

    added DefectInquiryDTO

---
+++ SQICS_Api/DTOs/DefectInquiryDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class DefectInquiryDTO
+    {
+        public string PiecePartCode { get; set; }
+
+        public string PiecePartLot { get; set; }
+
+        public string SubAssyCode { get; set; }
+
+        public string SubAssyLot { get; set; }
+
+        public string DefectName { get; set; }
+
+        public string ShiftId { get; set; }
+
+        public string ProdDate { get; set; }
+    }
+}
