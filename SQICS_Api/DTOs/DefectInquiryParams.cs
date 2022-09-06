6e22c73f1280656aefd03f498d33ec9f9509028f
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-09-06 09:18:57 +0800

    added DefectInquiryParams

---
+++ SQICS_Api/DTOs/DefectInquiryParams.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class DefectInquiryParams
+    {
+        [Required]
+        public int? SupplierId { get; set; }
+
+        [Required]
+        public DateTime? DateFrom { get; set; }
+
+        [Required]
+        public DateTime? DateTo { get; set; }
+
+        public int? DefectId { get; set; }
+
+        public int? ShiftId { get; set; }
+
+    }
+}
