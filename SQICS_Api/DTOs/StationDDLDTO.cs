22fe06b4da2f3b8725459f8dad66229d0b57bf89
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-09-13 10:17:43 +0800

    changed Id to Order in StationDDLDTO

--- SQICS_Api/DTOs/StationDDLDTO.cs
+++ SQICS_Api/DTOs/StationDDLDTO.cs
@@ -7,7 +7,7 @@ namespace SQICS_Api.DTOs
 {
     public class StationDDLDTO
     {
-        public int Id { get; set; }
+        public int Order { get; set; }
 
         public string StationName { get; set; }
     }
