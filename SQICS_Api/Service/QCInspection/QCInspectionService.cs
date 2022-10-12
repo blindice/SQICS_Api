d7d82e77318d5921064c3bc1559435e77e9681a3
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-12 09:22:11 +0800

    added QCInpection svc

---
+++ SQICS_Api/Service/QCInspection/QCInspectionService.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using SQICS_Api.UOW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Service.QCInspection
+{
+    public class QCInspectionService
+    {
+        IUnitOfWork _uow;
+        IMapper _mapper;
+
+        public QCInspectionService(IUnitOfWork uow, IMapper mapper)
+        {
+            _uow = uow;
+            _mapper = mapper;
+        }      
+    }
+}
