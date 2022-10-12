284371c4737d70a22a49850ec84db0b9cfc4289d
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-12 10:17:27 +0800

    added QCInspectionProfile

---
+++ SQICS_Api/Profiles/QCInspection/QCInspectionProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SQICS_Api.DTOs;
+using SQICS_Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Profiles.QCInspection
+{
+    public class QCInspectionProfile : Profile
+    {
+        public QCInspectionProfile()
+        {
+            CreateMap<AddQCInspectionDTO, tbl_t_lot_inspection>().ReverseMap();
+        }
+    }
+}
