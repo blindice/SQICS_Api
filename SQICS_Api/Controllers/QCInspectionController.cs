ede5b58b99e39ac73bce66ba81b1b1f356560896
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-12 09:51:30 +0800

    added AddInspectionAsync in QCinspection controller

--- SQICS_Api/Controllers/QCInspectionController.cs
+++ SQICS_Api/Controllers/QCInspectionController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SQICS_Api.DTOs;
+using SQICS_Api.Model;
+using SQICS_Api.Service.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +14,20 @@ namespace SQICS_Api.Controllers
     [ApiController]
     public class QCInspectionController : ControllerBase
     {
+        IQCInspectionService _service;
+
+        public QCInspectionController(IQCInspectionService service) => _service = service;
+
+        [HttpPost("addqcinspection")]
+        public async Task<IActionResult> AddQCInspectionAsync([FromBody] AddQCInspectionDTO inspection)
+        {
+            if (!ModelState.IsValid || inspection is null)
+                return BadRequest(new ErrorDetails { Message = "Invalid Transaction!", StatusCode = 400 });
+
+            await _service.AddInspectionAsync(inspection);
+
+            return Ok();
+        }
+
     }
 }
