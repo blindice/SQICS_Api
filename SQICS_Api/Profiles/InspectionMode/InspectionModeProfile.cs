2b1f6a982b09c681437b4761a8de0c3089370413
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-12 13:28:54 +0800

    added InspectionModeProfile

---
+++ SQICS_Api/Profiles/InspectionMode/InspectionModeProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SQICS_Api.DTOs;
+using SQICS_Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.Profiles.InspectionMode
+{
+    public class InspectionModeProfile : Profile
+    {
+        public LoginProfile()
+        {
+            CreateMap<tbl_m_inspection_mode, InspectionModeDDLDTO>().ReverseMap();
+        }
+    }
+}
