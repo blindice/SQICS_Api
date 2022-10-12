74e12caf69cc3bd4ba6cde51a54c1a9fa19339e5
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-12 13:27:27 +0800

    added InspectionModeDDLDTO

---
+++ SQICS_Api/DTOs/InspectionModeDDLDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQICS_Api.DTOs
+{
+    public class InspectionModeDDLDTO
+    {
+        public int fld_id { get; set; }
+        public string fld_description { get; set; }
+    }
+}
